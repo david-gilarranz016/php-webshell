b50cfbffa19c31a085c06a80199829b7930a04a8
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-05-31 21:04:54 +0200

    HistoryService -> Allows searching for commands

--- src/HistoryService.php
+++ src/HistoryService.php
@@ -3,6 +3,28 @@ namespace WebShell;
 
 class HistoryService extends Singleton
 {
+    private $history = [];
 
+    public function addCommand($cmd)
+    {
+        array_push($this->history, $cmd);
+    }
+
+    public function getHistory()
+    {
+        return $this->history;
+    }
+
+    public function searchCommand($cmd)
+    {
+        // Filter the $history array looking for commands that start with $cmd
+        $searchResults = array_filter($this->history, function ($savedCommand) use ($cmd)
+        {
+            return str_starts_with($savedCommand, $cmd);
+        });
+
+        // Reindex the array
+        return array_values($searchResults);
+    }
 }
 ?>
