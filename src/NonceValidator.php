067a47cba522058fbb79a0073f63caa9671e294a
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-03 19:19:23 +0200

    NonceValidator -> Updates nonce if the validation is successful

--- src/NonceValidator.php
+++ src/NonceValidator.php
@@ -7,10 +7,20 @@ class NonceValidator implements Validator
     {
         // Check that the request contains a body, it is an array, it contains a nonce
         // and the nonce is the one stored in the SecurityService
-        return array_key_exists('body', $request) &&
-            is_array($request['body']) &&
-            array_key_exists('nonce', $request['body']) &&
-            SecurityService::getInstance()->getNonce() == $request['body']['nonce'];
+        $instance = SecurityService::getInstance();
+        $valid = array_key_exists('body', $request)          &&
+                 is_array($request['body'])                  &&
+                 array_key_exists('nonce', $request['body']) &&
+                 $instance->getNonce() == $request['body']['nonce'];
+
+        // If valid, update the nonce
+        if ($valid) {
+            $nonce = random_bytes(16);
+            $instance->setNonce($nonce);
+        }
+
+        // Return the validation result
+        return $valid;
     }
 }
 ?>
--- tests/NonceValidatorTest.php
+++ tests/NonceValidatorTest.php
@@ -5,6 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 class NonceValidatorTest extends TestCase
 {
+    use \phpmock\phpunit\PHPMock;
+
+    /**
+     * Define the random_bytes function mock before running the class to fix Bug #68541
+     *
+     * @beforeClass
+     */ 
+    public function setUp(): void
+    {
+        $this->defineFunctionMock(__NAMESPACE__, 'random_bytes');
+    }
+
     public function testImplementsValidatorInterface(): void
     {
         // Get a validator instance
@@ -16,7 +28,7 @@ class NonceValidatorTest extends TestCase
 
     public function testValidatesNonceAgainstSecurityService(): void
     {
-        // Generate a random nonce and initialize the sercurity service
+        // Generate a random nonce and initialize the security service
         $nonce = random_bytes(16);
         SecurityService::getInstance()->setNonce($nonce);
 
@@ -31,7 +43,7 @@ class NonceValidatorTest extends TestCase
 
     public function testValidatesNonceAgainstSecurityServiceAndRejectsNonValidRequests(): void
     {
-        // Generate a random nonce and initialize the sercurity service
+        // Generate a random nonce and initialize the security service
         $nonce = random_bytes(16);
         SecurityService::getInstance()->setNonce($nonce);
 
@@ -42,6 +54,41 @@ class NonceValidatorTest extends TestCase
         // Validate the request and expect it not to be valid
         $valid = $validator->validate($request);
         $this->assertFalse($valid);
+    }
+
+    public function testUpdatesNonceIfRequestIsValid(): void
+    {
+        // Generate a random nonce and initialize the security service
+        $nonce = '0x92847f7ed261cc4d7ec0dacb6310b1db';
+        SecurityService::getInstance()->setNonce($nonce);
+
+        // Mock the random_bytes function to return a canned result
+        $newNonce = '0x335513fa6154be4225a22befb24cd852';
+        $random_bytes = $this->getFunctionMock(__NAMESPACE__, "random_bytes");
+        $random_bytes->expects($this->once())->with(16)->willReturn($newNonce);
+
+        // Create and validate a valid request
+        $request = [ 'body' => [ 'nonce' => $nonce ] ];
+        $validator = new NonceValidator;
+        $validator->validate($request);
+
+        // Expect the SecurityService's nonce to have been updated
+        $this->assertEquals($newNonce, SecurityService::getInstance()->getNonce());
+    }
+
+    public function testDoesNotUpdateNonceIfRequestIsValid(): void
+    {
+        // Generate a random nonce and initialize the security service
+        $nonce = random_bytes(16);
+        SecurityService::getInstance()->setNonce($nonce);
+
+        // Create and validate an invalid request
+        $request = [ 'body' => [ 'nonce' => 'non-valid-nonce' ] ];
+        $validator = new NonceValidator;
+        $validator->validate($request);
+
+        // Expect the SecurityService's nonce keep its original value
+        $this->assertEquals($nonce, SecurityService::getInstance()->getNonce());
     }
 }
 ?>
