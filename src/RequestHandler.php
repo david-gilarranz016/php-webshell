3e9ba66f85cb2a2a87f225dc847466cd3f1dddb3
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-03 21:04:23 +0200

    RequestHandler -> Basic skeleton for happy path created

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -5,8 +5,29 @@ class RequestHandler extends Singleton
 {
     private $actions = [];
 
+    public function addAction(string $key, Action $action): void
+    {
+        $this->actions[$key] = $action;
+    }
+
     public function handle(): string
     {
+        // Security service instance
+        $securityService = SecurityService::getInstance();
+
+        // Decrypt the request
+        $iv = base64_decode($_POST['iv']);
+        $body = $securityService->decrypt($_POST['body'], $iv);
+
+        // Parse the body
+        $request = json_decode($body);
+
+        // Check if there is an appropriate handler configured
+        if (array_key_exists($request->action, $this->actions)) {
+            // Call the appropriate action
+            $this->actions[$request->action]->run($request->args);
+        }
+
         // Set the content-type header
         header('Content-Type: application/json');
         return '';
--- tests/RequestHandlerTest.php
+++ tests/RequestHandlerTest.php
@@ -7,6 +7,17 @@ class RequestHandlerTest extends TestCase
 {
     use \phpmock\phpunit\PHPMock;
 
+    public function setUp(): void
+    {
+        // Initialize Security service
+        $key = random_bytes(32);
+        SecurityService::getInstance()->setKey($key);
+
+        // Initialize $_POST to an empty request to avoid errors on tests that do not
+        // provide a specific request
+        $this->createRequest(['action' => null]);
+    }
+
     public function tearDown(): void
     {
         // Clear all added actions
@@ -15,6 +26,9 @@ class RequestHandlerTest extends TestCase
         $actions = $reflectedInstance->getProperty('actions');
         $actions->setAccessible(true);
         $actions->setValue($instance, []);
+
+        // Reset modified global variables
+        $_POST = [];
     }
 
     public function testIsSingleton(): void
@@ -34,6 +48,34 @@ class RequestHandlerTest extends TestCase
 
         // Call the handle() method
         RequestHandler::getInstance()->handle();
+    }
+
+    public function testRedirectsRequestToAppropriateAction(): void
+    {
+        // Initialize variables
+        $key = 'test';
+        $args = (object) [ 'argument' => 'value' ];
+
+        // Create a sample action and expect it to be called with the created arguments
+        $action = $this->createMock(Action::class);
+        $action->expects($this->once())->method('run')->with($args)->willReturn('');
+
+        // Create a test request
+        $this->createRequest([ 'action' => $key, 'args' => $args ]);
+
+        // Add a request handler for the specified action
+        $instance = RequestHandler::getInstance();
+        $instance->addAction($key, $action);
+
+        // Call the handle() method and expect the mock action to have been called
+        $instance->handle();
+    }
+
+    private function createRequest(array $body): void
+    {
+        // Set the POST 'body' and 'iv' parameters to the result of json_encoding and 
+        // encrypting the supplied array
+        $_POST = SecurityService::getInstance()->encrypt(json_encode($body));
     }
 }
 ?>
