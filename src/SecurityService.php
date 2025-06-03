396b4301988dd4388299596c462e1c3f42da3b8b
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-03 18:32:47 +0200

    SecurityService -> decrypts messages

--- src/SecurityService.php
+++ src/SecurityService.php
@@ -3,6 +3,16 @@ namespace WebShell;
 
 class SecurityService extends Singleton
 {
+    private $key;
 
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function decrypt(string $body, string $iv): string
+    {
+        return openssl_decrypt($body, 'aes-256-cbc', $this->key, 0, $iv); 
+    }
 }
 ?>
--- tests/SecurityServiceTest.php
+++ tests/SecurityServiceTest.php
@@ -13,5 +13,43 @@ class SecurityServiceTest extends TestCase
         // Expect the intance to be a Singleton
         $this->assertInstanceOf(Singleton::class, $instance);
     }
+
+    public function testUsesSuppliedKeyToDecryptRequestBody()
+    {
+        // Generate a 256 bit key (32 bytes) for usage with AES-256 and a 128 bit (16 byte) initialization vector
+        $key = random_bytes(32);
+        $iv = random_bytes(16);
+
+        // Create and Encrypt a sample body (base64 encoded AES encrypted message)
+        $body = 'This is an example';
+        $encryptedBody = openssl_encrypt($body, 'aes-256-cbc', $key, 0, $iv);
+
+        // Initialize the SecurityService
+        $instance = SecurityService::getInstance();
+        $instance->setKey($key);
+
+        // Decrypt the body and verify the result
+        $result = $instance->decrypt($encryptedBody, $iv);
+        $this->assertEquals($body, $result);
+    }
+
+    public function testReturnsEmptyStringIfBodyCannotBeDecrypted()
+    {
+        // Generate a 256 bit key (32 bytes) for usage with AES-256 and a 128 bit (16 byte) initialization vector
+        $key = random_bytes(32);
+        $iv = random_bytes(16);
+
+        // Create and Encrypt a sample body with a different key
+        $body = 'This is an example';
+        $encryptedBody = openssl_encrypt($body, 'aes-256-cbc', random_bytes(32), 0, $iv);
+
+        // Initialize the SecurityService
+        $instance = SecurityService::getInstance();
+        $instance->setKey($key);
+
+        // Decrypt the body and verify the result
+        $result = $instance->decrypt($encryptedBody, $iv);
+        $this->assertEmpty($result);
+    }
 }
 ?>
