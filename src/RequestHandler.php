6bc203b02c5791727ac8e3c4867630e7b0b991b3
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-03 19:59:24 +0200

    RequestHandler -> adds Content-Type header

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -3,6 +3,13 @@ namespace WebShell;
 
 class RequestHandler extends Singleton
 {
+    private $actions = [];
 
+    public function handle(): string
+    {
+        // Set the content-type header
+        header('Content-Type: application/json');
+        return '';
+    }
 }
 ?>
--- tests/RequestHandlerTest.php
+++ tests/RequestHandlerTest.php
@@ -5,6 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 class RequestHandlerTest extends TestCase
 {
+    use \phpmock\phpunit\PHPMock;
+
+    public function tearDown(): void
+    {
+        // Clear all added actions
+        $instance = RequestHandler::getInstance();
+        $reflectedInstance = new \ReflectionObject($instance);
+        $actions = $reflectedInstance->getProperty('actions');
+        $actions->setAccessible(true);
+        $actions->setValue($instance, []);
+    }
+
     public function testIsSingleton(): void
     {
         // Get a Request Handler instance
@@ -12,6 +24,16 @@ class RequestHandlerTest extends TestCase
 
         // Expect it to be a Singleton
         $this->assertInstanceOf(Singleton::class, $instance);
+    }
+
+    public function testAddsJsonContentTypeHeader(): void
+    {
+        // Mock the headers function 
+        $headers = $this->getFunctionMock(__NAMESPACE__, 'header');
+        $headers->expects($this->once())->with('Content-Type: application/json');
+
+        // Call the handle() method
+        RequestHandler::getInstance()->handle();
     }
 }
 ?>
