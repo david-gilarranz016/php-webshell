142694aab1dcbccbb27cef4758c3c773c5d9e541
David Gilarraz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-07 16:56:04 +0200

    IdentifyExecutionAlternativesStep -> implemented

--- src/IdentifyExecutionAlternativesStep.php
+++ src/IdentifyExecutionAlternativesStep.php
@@ -3,9 +3,25 @@ namespace WebShell;
 
 class IdentifyExecutionAlternativesStep implements Step
 {
+    private $executionMethods;
+
+    public function __construct(array $executionMethods)
+    {
+        $this->executionMethods = $executionMethods;
+    }
+
     public function run(): void
     {
+        // Attempt to identify a valid execution method
+        $found = false;
 
+        // Loop until a valid execution method is found
+        for ($i = 0; $i < sizeof($this->executionMethods) && !$found; $i++) {
+            if ($this->executionMethods[$i]->isAvailable()) {
+                SystemService::getInstance()->setExecutionMethod($this->executionMethods[$i]);
+                $found = true;
+            }
+        }
     }
 }
 ?>
--- tests/IdentifyExecutionAlternativesStepTest.php
+++ tests/IdentifyExecutionAlternativesStepTest.php
@@ -5,13 +5,55 @@ use PHPUnit\Framework\TestCase;
 
 class IdentifyExecutionAlternativesStepTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        // Reset the SystemService to it's original state
+        $systemService = SystemService::getInstance();
+        $reflectedInstance = new \ReflectionObject($systemService);
+        $executionMethod = $reflectedInstance->getProperty('executionMethod');
+        $executionMethod->setAccessible(true);
+        $executionMethod->setValue($systemService, null);
+    }
+
     public function testImplementsStepInterface(): void
     {
         // Get a step instance
-        $step = new IdentifyExecutionAlternativesStep();
+        $step = new IdentifyExecutionAlternativesStep([]);
 
         // Expect the instance to be a Step
         $this->assertInstanceOf(Step::class, $step);
+    }
+
+    public function testItAddsTheFirstValidExecutionAlternativeToSysteService(): void
+    {
+        // Create a list of execution methods
+        $expectedOutput = 'Sample output';
+        $executionMethods = $this->createExecutionMethods([false, false, true, true], ['1', '2', $expectedOutput, '3']);
+
+        // Create and run the Step
+        $step = new IdentifyExecutionAlternativesStep($executionMethods);
+        $step->run();
+
+        // Run a command and expect the third execution method to be used
+        $output = SystemService::getInstance()->execute('test');
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    private function createExecutionMethods(array $validMethods, array $outputs): array
+    {
+        $executionMethods = [];
+
+        for ($i = 0; $i < sizeof($validMethods) && $i < sizeof($outputs); $i++) {
+            // Create a Mock execution method
+            $executionMethod = $this->createMock(ExecutionMethod::class);
+            $executionMethod->method('isAvailable')->willReturn($validMethods[$i]);
+            $executionMethod->method('execute')->willReturn($outputs[$i]);
+
+            // Add to the list
+            array_push($executionMethods, $executionMethod);
+        }
+
+        return $executionMethods;
     }
 }
 ?>
